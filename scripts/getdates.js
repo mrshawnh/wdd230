746a77dff07a237bc972d4db6804ad6128e2ff66
mrshawnh <hen23020@byui.edu>
2024-02-24 22:43:42 -0700

    Finished Week05 assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,13 @@ document.querySelector('#year').textContent = new Date().getFullYear();
 let lastModified = new Date(document.lastModified);
 
 document.querySelector('#lastModified').textContent = lastModified;
+
+/* Hamburger Menu */
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
