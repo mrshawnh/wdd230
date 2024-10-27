dc7baedbef627dcf15c51a2869b42a61b62a90c0
mrshawnh <hen23020@byui.edu>
2024-10-27 11:49:03 -0600

    link.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
-const baseURL = 'https://github.com.io/mrshawnh/wdd230/';
-const linksURL = 'https://github.com.io/mrshawnh/wdd230/data/links.json';
+const baseURL = 'https://mrshawnh.github.io/wdd230//wdd230/';
+const linksURL = 'https://mrshawnh.github.io/wdd230/data/links.json';
 
 async function getLinks() {
     const response = await fetch(linksURL);
