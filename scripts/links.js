2fa47632532903949becc4e3328ebd10d5d382f0
mrshawnh <hen23020@byui.edu>
2024-10-27 11:39:03 -0600

    updated links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,10 @@
-const
+const baseURL = 'https://github.com/mrshawnh/wdd230';
+const linksURL = 'https://github.com/mrshawnh/wdd230/blob/main/data/links.json';
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
