5f7fcc636da49343d6f653f58ffc297bbd64d841
mrshawnh <hen23020@byui.edu>
2024-10-27 13:34:01 -0600

    directory.js v1

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,10 @@
 const directory = document.querySelector("#directory");
 
 const json = "https://mrshawnh.github.io/wdd230/chamber/members.json";
+
+async function getMembers() {
+    const response = await fetch(json);
+    const data = await response.json();
+    console.log(data);
+    
+}
