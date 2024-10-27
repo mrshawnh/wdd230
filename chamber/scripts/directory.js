438edcb17bbdbdb104adc473f4f3b6d53f3b194a
mrshawnh <hen23020@byui.edu>
2024-10-27 13:45:52 -0600

    members.json update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,7 +6,26 @@ async function getMembers() {
     const response = await fetch(json);
     const data = await response.json();
     console.log(data);
+    displayMembers(data.members);
     
 }
 
+function displayMembers(members) {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let address = document.createElement('h3');
+        let phoneNumber = document.createElement('h3');
+        let website = document.createElement('a');
+        let logo = document.createElement('img');
+
+        name.textContent = member.name;
+
+        logo.setAttribute('src' , member.logo);
+        logo.setAttribute('alt', `Logo of ${member.name}`);
+        logo.setAttribute('width', '300');
+        logo.setAttribute('height', '300');
+
+    });
+}
 getMembers();
