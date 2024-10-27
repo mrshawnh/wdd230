db6bcc076827a151d250619f35602f1339ac6014
mrshawnh <hen23020@byui.edu>
2024-10-27 13:54:00 -0600

    directory progress

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -12,20 +12,32 @@ async function getMembers() {
 
 function displayMembers(members) {
     members.forEach((member) => {
-        let card = document.createElement('section');
+        let card = document.createElement('div');
         let name = document.createElement('h2');
-        let address = document.createElement('h3');
-        let phoneNumber = document.createElement('h3');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
         let website = document.createElement('a');
         let logo = document.createElement('img');
 
         name.textContent = member.name;
+        address.textContent = member.address;
+        phoneNumber.textContent = member.phone;
+        website.textContent = member.website;
 
         logo.setAttribute('src' , member.logo);
         logo.setAttribute('alt', `Logo of ${member.name}`);
         logo.setAttribute('width', '300');
         logo.setAttribute('height', '300');
+        website.setAttribute('href', member.website);
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(website);
+        directory.appendChild(card);
 
     });
 }
+
 getMembers();
