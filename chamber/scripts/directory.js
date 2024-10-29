ccdd4dbff9e9b498e719eeff43bcee611661ed47
mrshawnh <hen23020@byui.edu>
2024-10-28 19:36:14 -0600

    finished chamber

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,5 @@
 const directory = document.querySelector("#directory");
+const spotlight1 = document.querySelector("#spotlight1");
 
 const json = "https://mrshawnh.github.io/wdd230/chamber/data/members.json";
 
@@ -7,6 +8,7 @@ async function getMembers() {
     const data = await response.json();
     console.log(data);
     displayMembers(data.members);
+    displaySpotlights(data.members);
     
 }
 
@@ -38,6 +40,26 @@ function displayMembers(members) {
         directory.appendChild(card);
 
     });
+}
+
+
+
+function displaySpotlights(members){
+    let name = document.createElement('h2');
+    let address = document.createElement('p');
+    let phoneNumber = document.createElement('p');
+    let website = document.createElement('a');
+
+    name.textContent = members[0].name;
+    address.textContent = members[0].address;
+    phoneNumber.textContent = members[0].phone;
+    website.textContent = members[0].website;
+    website.setAttribute('href', member.website);
+
+    spotlight1.appendChild(name);
+    spotlight1.appendChild(address);
+    spotlight1.appendChild(phoneNumber);
+    spotlight1.appendChild(website);
 }
 
 getMembers();
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,7 +2,12 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.61&lon=-116.20&appid=242c69025911a3a0ade03b01b670043d&units=imperial';
+const forecast1 = document.querySelector("#forecast1");
+const forecast2 = document.querySelector("#forecast2");
+const forecast3 = document.querySelector("#forecast3");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=13.33&lon=122.01&appid=242c69025911a3a0ade03b01b670043d&units=imperial';
+const forecasturl = 'https://api.openweathermap.org/data/2.5/forecast?lat=13.33&lon=122.01&appid=242c69025911a3a0ade03b01b670043d&units=imperial'
                 
 async function apiFetch() {
     try{
@@ -31,3 +36,20 @@ function displayResults(data){
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
+
+async function apiFetchForecast() {
+    try{
+        const response = await fetch(forecasturl);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error){
+        console.log(error);
+    }
+    
+}
+
+apiFetchForecast()
